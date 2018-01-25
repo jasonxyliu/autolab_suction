9f285ca4003be7caabd88a40ca1b7308ffb843c5
jeff <jmahler@berkeley.edu>
2018-01-24 23:21:39 -0800

    Added suction toggle scripts

---
+++ ros_nodes/suction_on.py
@@ -0,0 +1,11 @@
+import rospy
+import time
+
+from autolab_suction.srv import Suction
+
+if __name__ == '__main__':
+    rospy.init_node('suction_toggle_node')
+    rospy.wait_for_service('toggle_suction')
+    toggle_suction = rospy.ServiceProxy('toggle_suction', Suction)
+    rospy.loginfo('Suction On')
+    toggle_suction(True)
